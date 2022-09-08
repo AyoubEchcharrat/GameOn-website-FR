e4ada235065d3d592e5fed0cbb85b62c91fc9a42
Ayoub Echcharrat <ayoub.ech-charrat@hotmail.fr>
2022-09-08 15:55:13 +0200

    ajout du script JS + correctifs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,9 +10,12 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBody = document.getElementById('modal-body');
 const formData = document.querySelectorAll(".formData");
 const closeModal = document.querySelector('.close');
 const modal = document.querySelector('.bground');
+const clickBtn = document.querySelector('.btn-submit');
+const form = document.getElementById('reserve')
 
 var firstName = document.getElementById('first');
 var lastName = document.getElementById('last');
@@ -34,62 +37,137 @@ closeModal.onclick = function () {
   }
 }
 
+// clear invalid msg every try
+clickBtn.onclick = function () {
+  const msg = document.querySelectorAll('.invalidMessage');
+  msg.forEach(msg => {
+    msg.remove();
+  });
+  verifyValidityFirst();
+  verifyValidityLast();
+  verifyValidityEmail();
+  verifyValidityBirthdate();
+  verifyValidityQuantity();
+  verifyValidityTerms();
+}
+
+// not refreshing page on submit
+function handleForm(e) {
+  e.preventDefault();
+  form.style.height = "0px";
+  form.style.visibility = "hidden";
+  modalBody.style.minHeight = "500px";
+  var thanks = document.createElement('div');
+  thanks.classList.add("thanks-message");
+  modalBody.appendChild(thanks).innerText = "Merci ! Votre réservation a été reçue.";
+
+} 
+form.addEventListener('submit', handleForm);
+
 
 // form invalid msg : prénom
-firstName.oninvalid = function(e) {
-	e.target.setCustomValidity("");
-	if (!e.target.validity.valid) {
-		if (e.target.value.length == 0) {
-e.target.setCustomValidity("Ce champ est obligatoire");
-		} else {
-e.target.setCustomValidity("Entrez un prénom correct");
-		}
-	}
-};
+function verifyValidityFirst() {
+  let text;
+  if (!firstName.validity.valid) {
+    firstName.setCustomValidity(" ");
+    var div = document.createElement('div');
+    div.classList.add("invalidMessage");
+    if (firstName.value.length < 2) {
+      text = "Ce champ est obligatoire (2 caractères minimum)";
+    } else {
+      firstName.setCustomValidity("");
+      text = "";
+    } 
+    document.getElementById('first').closest('.formData').appendChild(div).innerText = text;
+  }
+}
+
+
 
 // form invalid msg : nom
-lastName.oninvalid = function(e) {
-	e.target.setCustomValidity("");
-	if (!e.target.validity.valid) {
-		if (e.target.value.length == 0) {
-e.target.setCustomValidity("Ce champ est obligatoire");
-		} else {
-e.target.setCustomValidity("Entrez un nom correct");
-		}
-	}
-};
+function verifyValidityLast() {
+  let text;
+  if (!lastName.validity.valid) {
+    lastName.setCustomValidity(" ");
+    var div = document.createElement('div');
+    div.classList.add("invalidMessage");
+    if (lastName.value.length < 2) {
+      text = "Ce champ est obligatoire (2 caractères minimum)";
+    } else {
+      lastName.setCustomValidity("");
+      text = "";
+    } 
+    document.getElementById('last').closest('.formData').appendChild(div).innerText = text;
+  }
+}
 
 
 // form invalid msg : email
-email.oninvalid = function(e) {
-	e.target.setCustomValidity("");
-	if (!e.target.validity.valid) {
-		if (e.target.value.length == 0) {
-e.target.setCustomValidity("Vous devez entrer votre adresse e-mail");
-		} else {
-e.target.setCustomValidity("Entrez une adresse e-mail valide");
-		}
-	}
-};
+function verifyValidityEmail() {
+  let text;
+  if (!email.validity.valid) {
+    email.setCustomValidity(" ");
+    var div = document.createElement('div');
+    div.classList.add("invalidMessage");
+    if (!email.value.includes("@")) {
+      text = "Ce champ est obligatoire (doit contenir un @)";
+    } else {
+      email.setCustomValidity("");
+      text = ""; 
+    } 
+    document.getElementById('email').closest('.formData').appendChild(div).innerText = text;
+  }
+}
+
+
 
 // form invalid msg : birthdate
 birthdate.max = new Date().toISOString().split("T")[0];
 
-birthdate.oninvalid = function(e) {
-	e.target.setCustomValidity("");
-	if (!e.target.validity.valid) {
-		if (e.target.value.length == 0) {
-e.target.setCustomValidity("Vous devez entrer votre date de naissance");
-		} else {
-e.target.setCustomValidity("Entrez une date valide");
-		}
-	}
-};
+function verifyValidityBirthdate() {
+  let text;
+  if (!birthdate.validity.valid) {
+    birthdate.setCustomValidity(" ");
+    var div = document.createElement('div');
+    div.classList.add("invalidMessage");
+    if (birthdate.value.length == 0) {
+      text = "Ce champ est obligatoire";
+    } else {
+      birthdate.setCustomValidity("");
+      text = "";
+    } 
+    document.getElementById('birthdate').closest('.formData').appendChild(div).innerText = text;
+  }
+}
+  
+
+
+// form invalid msg : quantity
+function verifyValidityQuantity() {
+  let text;
+  if (!quantity.validity.valid) {
+    quantity.setCustomValidity(" ");
+    var div = document.createElement('div');
+    div.classList.add("invalidMessage");
+    if (quantity.value.length == 0) {
+      text = "Ce champ est obligatoire";
+    } else {
+      quantity.setCustomValidity("");
+      text = "";
+    } 
+    document.getElementById('quantity').closest('.formData').appendChild(div).innerText = text;
+  }
+}
 
 // form invalid msg : terms
-condition.oninvalid = function(e) {
-	e.target.setCustomValidity("");
-	if (!e.target.validity.valid) {
-e.target.setCustomValidity("Vous devez entrer votre date de naissance");
-	}
-};
+function verifyValidityTerms() {
+  let text;
+  if (!condition.validity.valid) {
+      condition.setCustomValidity(" ");
+      var div = document.createElement('div');
+      div.classList.add("invalidMessage");
+      text = "Ce champ est obligatoire";
+      document.getElementById('checkbox1').closest('.formData').appendChild(div).innerText = text;
+  }
+  condition.setCustomValidity("");
+  }
